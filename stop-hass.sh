f42bb391c3b1b4c612bd654713ed7d635c2da139
Henrik Brautaset Aronsen <henrik@synth.no>
2019-02-08 15:26:24 +0100

    things suddenly have new id's, making heaters not work :facepalm:

--- stop-hass.sh
+++ stop-hass.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 PIDFILE=$HOME/.homeassistant/homeassistant.pid
+
+if [[ ! -f $PIDFILE ]]; then
+  return
+fi
+
 PID=$(cat $PIDFILE)
+
+if [[ -z "$PID" ]]; then
+  return
+fi
 
 while true; do
   ps ax |grep "^ *$PID " >/dev/null
